8df692e4f47f9bf3cae3cb0cfc248f03bc20adb5
AashiqinCode <aashiqahmed10101@gmail.com>
2020-09-03 20:11:53 +0530

    implemented Redux persist and done some design stuffs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
-import { Provider } from "react-redux";
-import { store } from "./redux";
 import "./App.css";
+import { Provider } from "react-redux";
+import {store,persistor} from './redux';
+import { PersistGate } from "redux-persist/integration/react";
+
 // Components
 import TodoInput from "./Components/Form";
 import TodoList from "./Components/ToDoList";
@@ -9,8 +11,11 @@ import TodoList from "./Components/ToDoList";
 const App = () => (
   <Provider store={store}>
     <div className="main">
-      <TodoInput />
-      <TodoList />
+      <header className="main">To Do List</header>
+      <PersistGate persistor={persistor}>
+        <TodoInput />
+        <TodoList />
+      </PersistGate>
     </div>
   </Provider>
 );
--- src/redux.js
+++ src/redux.js
@@ -1,5 +1,12 @@
 import { createStore } from "redux";
 import { v4 as uuid } from "uuid";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
 
 const initialState = {
   todos: [
@@ -16,16 +23,27 @@ const initialState = {
   ],
 };
 
-export const store = createStore(
-  reducer,
-  initialState, //As i didnt used CombinedReducer,passed the initial state to the createStore
-  typeof window === "object" &&
-    typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== "undefined"
-    ? window.__REDUX_DEVTOOLS_EXTENSION__()
-    : (f) => f
-);
+// Persisted Reducer
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+// Actions
+export const addTodoAction = (todo) => ({
+  type: "ADD_TODO",
+  payload: todo,
+});
+
+export const toggleTodoComplete = (todoId) => ({
+  type: "TOGGLE_TODO",
+  payload: todoId,
+});
+
+export const deleteTodoAction = (todoId) => ({
+  type: "DELETE_TODO",
+  payload: todoId,
+});
+
 // Reducer
-function reducer(state, action) {
+export function reducer(state, action) {
   switch (action.type) {
     case "ADD_TODO":
       return {
@@ -53,18 +71,13 @@ function reducer(state, action) {
       return state;
   }
 }
-// Actions
-export const addTodoAction = (todo) => ({
-  type: "ADD_TODO",
-  payload: todo,
-});
 
-export const toggleTodoComplete = (todoId) => ({
-  type: "TOGGLE_TODO",
-  payload: todoId,
-});
-
-export const deleteTodoAction = (todoId) => ({
-  type: "DELETE_TODO",
-  payload: todoId,
-});
+export const store = createStore(
+  persistedReducer,
+  initialState,
+  typeof window === "object" &&
+    typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== "undefined"
+    ? window.__REDUX_DEVTOOLS_EXTENSION__()
+    : (f) => f
+);
+export const persistor = persistStore(store);
