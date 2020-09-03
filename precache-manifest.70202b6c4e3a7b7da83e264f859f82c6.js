0fce7f454f27989f405e1280835844f0637ab903
AashiqinCode <aashiqahmed10101@gmail.com>
2020-09-03 22:12:54 +0530

    Updates

---
+++ precache-manifest.70202b6c4e3a7b7da83e264f859f82c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32f3b94d18f46b89350c8a6bbcc64a03",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "124cc37035e85c84a0e7",
+    "url": "/todoapp/static/css/main.8a6d6d49.chunk.css"
+  },
+  {
+    "revision": "222dd0765a8d254c3f8b",
+    "url": "/todoapp/static/js/2.d6ea7f7f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todoapp/static/js/2.d6ea7f7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "124cc37035e85c84a0e7",
+    "url": "/todoapp/static/js/main.7ffafaac.chunk.js"
+  },
+  {
+    "revision": "80b05f8f46ecf0bf2c2b",
+    "url": "/todoapp/static/js/runtime-main.5545d9fc.js"
+  }
+]);
